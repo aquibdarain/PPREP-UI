c09c5c9e2d7c1cfd0cac11d8543c14516924a2eb
Aquib Darain <darainaquib4@gmail.com>
2023-04-24 13:22:07 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './home/landing-page/landing-page.component';
 
 const routes: Routes = [
   {
-    path:'',redirectTo:'landing', pathMatch:'full',
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full',
   },
   {
     path: 'landing',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  }
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { Routes, RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: '',
     redirectTo: 'page',
@@ -17,7 +27,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [LandingPageComponent, FooterComponent],
+  declarations: [
+    LandingPageComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class HomeModule {}
